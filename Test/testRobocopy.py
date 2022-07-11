fba786d0c7c1d37cb2fb4c18b39c20784ba7e381
tankin <tnag@163.com>
2022-07-11 14:28:43 +0800

    Update testRobocopy.py

--- Test/testRobocopy.py
+++ Test/testRobocopy.py
@@ -1,9 +1,13 @@
 # -*- coding: UTF-8 -*-
 
-from lib import CommonFunc
 import os
 import time
 
+def RunCmd(cmd):
+    print(cmd)
+    code = os.system(cmd)
+    return code
+    
 if __name__ == "__main__" :
     fromDir = "e:\\"
     toDir = "f:\\"
@@ -18,7 +22,7 @@ if __name__ == "__main__" :
 
     #cmd = "robocopy %s %s %s" % (fromDir, toDir, fileName)
     cmd = "copy %s\\%s %s\\%s" % (fromDir, fileName, toDir, fileName)
-    CommonFunc.RunCmd(cmd)
+    RunCmd(cmd)
 
     total = int(round((time.time()-t) * 1000))    
     print("time: %d" %(total))
